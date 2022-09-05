457ff4dfb98b40cf6b62f259484c1bea6b65b121
pierre allison <pierreallison@gmail.com>
2022-09-05 17:50:48 +0200

    installation header et bootstrap

--- readme.md
+++ readme.md
@@ -1,12 +1,13 @@
 # Espigoule generator
 
-Espigoule Generator est un site web mettant à la disposition de toutes et tous les 
-citations les plus croustillantes du célèbre documentaire.
+Espigoule Generator est un site web mettant à la disposition de toutes et tous les citations les plus croustillantes du célèbre documentaire.
 
 ## Installation
 
-Use the package manager [pip](https://pip.pypa.io/en/stable/) to install foobar.
-
-```bash
-pip install foobar
-```
+* en ligne de commande :
+- composer install
+- npm install
+- npm run build
+- Lancer xampp
+- php -S localhost:8000 dans le terminal
+- accéder au site avec l'url : http://localhost:8000/public/home
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,39 @@
 
 namespace App\Controller;
 
+use App\Entity\Clip;
+use App\Repository\ClipRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route('/home', name: 'app_home')]
+    /*#[Route('/home', name: 'app_home')]
     public function index(): Response
     {
+        //Je mets dans une variable la liste de mes clips
+        $clipsList = $Cli
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+        ]);
+    }*/
+
+
+    /**
+     * @Route("/home", name="main_home")
+     */
+    public function home(ClipRepository $clipRepository, Clip $clip = null)
+    {
+        // On va chercher les données
+        $clipsList = $clipRepository->findAll();
+
+        // On rend un template Twig à partir du dossier templates/
+        // en lui donnant les données nécessaires
+        return $this->render('home/index.html.twig', [
+            // Variable Twig => variable PHP
+            'clip' => $clip,
+            'clipsList' => $clipsList,
         ]);
     }
 }
